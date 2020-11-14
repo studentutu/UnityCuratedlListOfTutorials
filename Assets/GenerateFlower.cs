43ba0162edfac9e1aec3ee39735370afbbf5efc9
Roman Fedorov <studentutu@gmail.com>
2020-11-14 19:33:11 +0200

    Lesson3 Input Procedural animation, Collision and Trigger

---
+++ Assets/GenerateFlower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class RandomInstantiate : MonoBehaviour 
+{
+    
+     public GameObject[] yourArray;
+ 
+ void Update()
+     {
+         if (Input.GetMouseButtonDown(0))
+         {
+             Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+ 
+             int index = Random.Range(0, yourArray.Length);
+    
+             Instantiate(yourArray[index], spawnPosition, Quaternion.identity);
+         }
+     }
+ }
