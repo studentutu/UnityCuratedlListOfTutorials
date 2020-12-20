d1830eca39a95ea3f1cdd84334516fe8419a9686
Roman Fedorov <studentutu@gmail.com>
2020-12-20 18:27:53 +0200

    Creat VR shooter. VRTK + grab + auto grab + interrractible object + interractible object with use

---
+++ TutorialPar2/Assets/App/Scripts/ShootFromWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFromWeapon : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody CloneObjetc;
+    [SerializeField] private float Impulse;
+    [SerializeField] private float TimeToLive;
+    public void InstantiedShoot(Transform Position)
+    {
+        var PosObj = GameObject.Instantiate(CloneObjetc);
+        PosObj.transform.position = Position.position;
+        PosObj.transform.rotation = Position.rotation;
+        PosObj.AddForce(PosObj.transform.forward*Impulse, ForceMode.Impulse) ;
+        Destroy(PosObj.gameObject , TimeToLive);
+    }
+}
---
+++ TutorialPar2/Assets/App/Scripts/StartedAutoGrab.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartedAutoGrab : MonoBehaviour
+{
+    [SerializeField] private float Second=2f;
+    [SerializeField] private UnityEvent StartAutoEvent;
+
+    private void Start()
+    {
+        StartCoroutine(StartAuto());
+    }
+
+    private IEnumerator StartAuto()
+    {
+        yield return new WaitForSeconds(Second);
+        StartAutoEvent?.Invoke();
+    }
+
+}
