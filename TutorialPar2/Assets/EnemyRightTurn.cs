69a5f193097e34175decdbcac96e0c09ea0de61f
Roman Fedorov <studentutu@gmail.com>
2020-12-27 16:56:46 +0200

    Fixed RegDoll

--- TutorialPar2/Assets/EnemyRightTurn.cs
+++ TutorialPar2/Assets/EnemyRightTurn.cs
@@ -1,72 +1,85 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class EnemyRightTurn : MonoBehaviour
 {
-    //-public Collider MainCollider;
-    //public Collider[] AllColliders;
-
-    //public void Awake()
-    //{
-    //    MainCollider = GetComponent<Collider>();
-    //    AllColliders = GetComponentsInChildren<Collider>(false);
-    //    DoRagdoll(false);
+    public Collider MainCollider;
+    public Collider[] AllColliders;
+    public Rigidbody rb;
+    public Animator Animator;
+    public void Awake()
+    {
         
-    //}
+        AllColliders = GetComponentsInChildren<Collider>(false);
+        DoRagdoll(false);
+        
+    }
 
     
 
-     //public void DoRagdoll(bool isRagdoll)
-     //{
-     //  
-     //    foreach (var col in AllColliders  )
-     //    {
-     //        col.enabled = isRagdoll;
-     //        MainCollider.enabled = !isRagdoll;
-     //        GetComponent<Rigidbody>().useGravity = !isRagdoll;
-     //        //GetComponent<Animator>().enabled = !isRagdoll;
+     public void DoRagdoll(bool isRagdoll)
+     {
+        rb.Sleep();
+         foreach (var col in AllColliders  )
+         {
+             col.enabled = isRagdoll;
+             MainCollider.enabled = !isRagdoll;
+             rb.useGravity = !isRagdoll;
+             Animator.enabled = !isRagdoll;
+    
+         }
 
-        // }
+         MainCollider.enabled = !isRagdoll;
+     }
+
+     private void OnCollisionEnter(Collision other)
+     {
+         DoRagdoll(true);
+         var WorldPosistion=other.contacts[0].point;
+         rb.AddForceAtPosition(other.impulse, WorldPosistion);
+     }
+
+     // [Header("Reference")] 
+    // [SerializeField] private Animator animator = null;
+    //
+    // private Rigidbody[] ragdollBodies;
+    // private Collider[] ragdollColliders;
+    //
+    // private void Start()
+    // {
+    //     ragdollBodies = GetComponentsInChildren<Rigidbody>();
+    //     ragdollColliders = GetComponentsInChildren<Collider>();
+    //     ToggleRagDoll(false);
+    //     //Invoke(nameof(Die), 2f);
     // }
-    [Header("Reference")] 
-    [SerializeField] private Animator animator = null;
-
-    private Rigidbody[] ragdollBodies;
-    private Collider[] ragdollColliders;
-
-    private void Start()
-    {
-        ragdollBodies = GetComponentInChildren<Rigidbody[]>();
-        ragdollColliders = GetComponentInChildren<Collider[]>();
-        ToggleRagDoll(true);
-        Invoke(nameof(Die), 2f);
-    }
-
-    private void Die()
-    {
-        foreach (Rigidbody rb in ragdollBodies)
-            
-        {
-            rb.AddExplosionForce(1f, new Vector3(1f,1f,1f),1f, 1f, ForceMode.Impulse );
-        }
-    }
-
-    private void ToggleRagDoll(bool state)
-    {
-        animator.enabled = !state;
-        foreach (Rigidbody rb in ragdollBodies)
-        {
-            rb.isKinematic = state;
-        }
-
-        foreach (Collider collider in ragdollColliders)
-        {
-            collider.enabled = state; 
-        }
-
-    }
+    //
+    // private async void Die()
+    // {
+    //     await Task.Delay(1000);
+    //     foreach (Rigidbody rb in ragdollBodies)
+    //         
+    //     {
+    //         rb.AddExplosionForce(1f, new Vector3(1f,1f,1f),1f, 1f, ForceMode.Impulse );
+    //     }
+    // }
+    //
+    // private void ToggleRagDoll(bool state)
+    // {
+    //     animator.enabled = !state;
+    //     foreach (Rigidbody rb in ragdollBodies)
+    //     {
+    //         rb.isKinematic = state;
+    //     }
+    //
+    //     foreach (Collider collider in ragdollColliders)
+    //     {
+    //         collider.enabled = state; 
+    //     }
+    //
+    // }
 }
 
 
