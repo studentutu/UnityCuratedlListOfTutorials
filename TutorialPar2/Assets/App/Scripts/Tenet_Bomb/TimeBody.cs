730b37c4ec15e7079502b1ba59ac5bfa3eed4dd6
Roman Fedorov <studentutu@gmail.com>
2021-01-09 20:44:08 +0200

    c#

--- TutorialPar2/Assets/App/Scripts/Tenet_Bomb/TimeBody.cs
+++ TutorialPar2/Assets/App/Scripts/Tenet_Bomb/TimeBody.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeBody : MonoBehaviour {
+	
+	Dictionary<string, int> DictionaryOfStrings = new Dictionary<string, int>();
 
 	bool isRewinding = false;
 
@@ -12,18 +14,67 @@ public class TimeBody : MonoBehaviour {
 
 	Rigidbody rb;
 
+	private System.Action<int> doSomething; // void anymethod(.....)
+	private System.Func<bool,string, int> getSomething;  // int
+
+	public static event System.Action OnUpdaetEvent;
+	
 	// Use this for initialization
 	void Start () {
 		pointsInTime = new List<PointInTime>();
 		rb = GetComponent<Rigidbody>();
+
+		doSomething = (index) => { };
+		getSomething = (anybool, anystring) =>
+		{
+			return anybool ? 1 : 0;
+		};
+		
+		DictionaryOfStrings.Add("Roman", 25);
+//		DictionaryOfStrings.Add("Roman", 25);
+		if (DictionaryOfStrings.ContainsKey("Roman"))
+		{
+			Debug.Log("Yes, " + DictionaryOfStrings["Roman"]);
+		}
+
+		DictionaryOfStrings.Remove("Roman");
+
+		var getfromDelegate = getSomething.Invoke(true, "");
+		CallMyself(0);
+		var GetFT = GetFromTuples();
+		
+		// anyST == Changred
 	}
-	
+
+	private System.Tuple<int, string> GetFromTuples()
+	{
+		
+		return new System.Tuple<int, string>(1, "Changed");
+	}
+
+
+	/// <summary>
+	/// any
+	/// </summary>
+	/// <param name="initial"></param>
+	private int CallMyself(int initial)
+	{
+		if (initial>10)
+		{
+			return initial;
+		}
+		Debug.Log(initial);
+		return CallMyself(initial + 1);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.F1))
 			StartRewind();
 		if (Input.GetKeyUp(KeyCode.F1))
 			StopRewind();
+		
+		OnUpdaetEvent?.Invoke();
 	}
 
 	void FixedUpdate ()
@@ -47,6 +98,7 @@ public class TimeBody : MonoBehaviour {
 		} else
 		{
 			StopRewind();
+			doSomething?.Invoke(4);
 		}
 		
 	}
