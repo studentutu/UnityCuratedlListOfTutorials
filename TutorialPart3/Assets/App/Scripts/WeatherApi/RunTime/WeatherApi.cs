d1df31a8a4d98c1fdb093e11670df7d06bd522ae
Roman Fedorov <studentutu@gmail.com>
2021-01-30 19:49:26 +0200

    Automatic tests WeatherAPI

---
+++ TutorialPart3/Assets/App/Scripts/WeatherApi/RunTime/WeatherApi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace CastomApi
+{
+    public class WeatherApi : MonoBehaviour
+    {
+        [SerializeField] private string ApiKeyPass;
+        [SerializeField] private string url;
+
+
+        [NonSerialized] private string CashApiKey = null; // fields
+
+        // Property
+        public string StoredApiKey
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(CashApiKey))
+                {
+                    var TestAssets = Resources.Load<TextAsset>(ApiKeyPass);
+                    CashApiKey = TestAssets.text;
+                }
+
+                return CashApiKey;
+            }
+        }
+        
+        public void GetWeather(string city, System.Action OnComplite)
+        {
+            StartCoroutine(WaitingForRespounse(city, OnComplite));
+        }
+
+        private IEnumerator WaitingForRespounse(string city, System.Action OnComplite)
+        {
+            string finalurl = string.Format(url, city, StoredApiKey);
+            using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(finalurl))
+            {
+                yield return unityWebRequest.SendWebRequest();
+
+                if (string.IsNullOrEmpty(unityWebRequest.error))
+                {
+                    // no errors
+                    Debug.Log(unityWebRequest.downloadHandler.text);
+                    var InternalObject = WeatherApiJson.FromJsdon(unityWebRequest.downloadHandler.text);
+                    Debug.Log(InternalObject.ToJson());
+                }
+                else
+                {
+                    Debug.Log(unityWebRequest.responseCode + " " + unityWebRequest.error);
+                }
+            }
+
+            OnComplite.Invoke();
+        }
+    }
+
+    [Serializable]
+    public class WeatherApiJson
+    {
+        public CoordsWeather coord;
+        public List<WeatherListItem> weather;
+
+        public static WeatherApiJson FromJsdon(string Json)
+        {
+            return JsonUtility.FromJson<WeatherApiJson>(Json);
+        }
+
+        public string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+
+    [Serializable]
+    public class CoordsWeather
+    {
+        public float lon;
+        public float lat;
+    }
+
+    [Serializable]
+    public class WeatherListItem
+    {
+        public int id;
+        public string main;
+        public string description;
+    }
+}
---
+++ TutorialPart3/Assets/App/Scripts/WeatherApi/Tests/WeatherApiTests/WeatherTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CastomApi;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class WeatherTest
+    {
+        private WeatherApi SetupTest()
+        {
+            var newObject = new GameObject();
+            var api =  newObject.AddComponent<WeatherApi>();
+            var apiOriginal = Resources.Load<WeatherApi>(nameof(WeatherApi));
+            var tpJson = JsonUtility.ToJson(apiOriginal);
+            JsonUtility.FromJsonOverwrite(tpJson, api);
+            
+            return api;
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void WeatherTest2SimplePasses()
+        {
+           var TestObject =  SetupTest();
+           Assert.IsNotNull(TestObject.StoredApiKey);
+           
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator WeatherTest2WithEnumeratorPasses()
+        {
+            var testObject = SetupTest();
+            bool cheking = false;
+            testObject.GetWeather("London", () =>
+            {
+                cheking = true;
+            });
+            while (!cheking)
+            {
+                yield return null;
+                
+            }
+
+            LogAssert.Expect(LogType.Log, new Regex("[0-1000]*"));
+
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+        }
+    }
+}
